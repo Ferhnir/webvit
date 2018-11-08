21832e02ff44de8e4188b0ddb5e56d04daba99f4
Ferhnir <zax1984@gmail.com>
2018-11-08 22:13:48 +0000

    Changed authorization for db settings

---
+++ app/config/settings.php
@@ -0,0 +1,28 @@
+<?php
+//loading priv file for db
+$priv_file = __DIR__ . '\.priv_data';
+if(file_exists($priv_file)){
+    $priv_data = explode(',',file_get_contents($priv_file));
+} else {
+    var_dump('.priv_data file in /config folder not found. This file must contain: "user,password" ');
+    die();
+}
+
+return [
+    'settings' => [
+        'displayErrorDetails' => true,
+        'addContentLengthHeader' => false,
+
+        'db' => [
+            'driver'        => 'mysql',
+            'host'          => 'localhost',
+            'database'      => 'toy',
+            'username'      => $priv_data[0],
+            'password'      => $priv_data[1],
+            'charset'       => 'utf8',
+            'collation'     => 'utf8_unicode_ci',
+            'prefix'        => ''
+        ],
+    ],
+];
+?>
