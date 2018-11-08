5f0d125132148f3e5beb3e3607e93206efa4940e
Ferhnir <zax1984@gmail.com>
2018-11-08 21:46:20 +0000

    Downloaded css and js libs. Added script and stilization for view

---
+++ app/public/js/app.js
@@ -0,0 +1,13 @@
+$('#datepicker_from').datepicker({
+    uiLibrary: 'bootstrap4'
+});
+$('#datepicker_to').datepicker({
+    uiLibrary: 'bootstrap4'
+});
+
+$(document).ready(function() {
+    
+    setTimeout(function(){
+        $("#flashAlert").fadeOut(1000);
+    },'4000');
+});
