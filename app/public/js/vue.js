e73ab8766af194f5bd4ef74f4509cf8cb95d967d
Maksymilian <zax1984@gmail.com>
2018-11-26 12:51:30 +0000

    Implementing vuejs to project

---
+++ app/public/js/vue.js
@@ -0,0 +1,12 @@
+var test = new Vue({
+    el: '#test',
+    data: {
+      message: 'Hello Vue!'
+    },
+    methods: {
+        reverseMessage: function () {
+         console.log('working');
+        }
+      }
+  });
+
