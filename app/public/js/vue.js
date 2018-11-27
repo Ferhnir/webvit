4a79761ca0fa68ebc08cd9d67b409c162b70b454
Maksymilian <zax1984@gmail.com>
2018-11-27 13:40:24 +0000

    Adding JS validation form for password change - part 1

--- app/public/js/vue.js
+++ app/public/js/vue.js
@@ -1,11 +1,51 @@
-var test = new Vue({
-  el: '#test',
-  data: {
-    message: 'Hello Vue!'
-  },
-  methods: {
-      reverseMessage: function () {
-       console.log('working');
+var passwordChangeVerification = new Vue({
+  el: '#passwordChangeVerification',
+  data() {
+    return {
+      submitBttn: false,
+      newPassword: '',
+      reNewPassword: '',
+      validations: {
+        notNull: false,
+        minLength: false,
+        samePassword: false,
+        minOneCapLetter: false,
+        minOneSpecialSign: false
       }
     }
+  },
+  methods: {
+    eventChange() {
+
+      this.validatePasswords();
+
+    },
+    validate() {
+      return this.validations.notNull && this.validations.minLength && this.validations.samePassword;
+    },
+    validatePasswords() {
+      
+      //not empty
+      this.newPassword.length > 0 ? this.validations.notNull = true : this.validations.notNull = false;
+      
+      //longer than 7
+      this.newPassword.length >= 8 ? this.validations.minLength = true : this.validations.minLength = false;
+      
+      //same passwords
+      this.newPassword == this.reNewPassword ? this.validations.samePassword = true : this.validations.samePassword = false;
+      
+      //has one capital letter
+      this.findCapLetter(this.newPassword,1) ? this.validations.minOneCapLetter = true : this.validations.minOneCapLetter = false;
+      
+      
+      this.submitBttn = this.validate();
+
+    },
+    findCapLetter(password, nr) {
+      return password.replace(/[a-z]/g, '').length >= nr;
+    },
+    findSpecialCharacter(password) {
+
+    }
+  }
 });
